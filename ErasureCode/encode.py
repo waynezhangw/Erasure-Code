7c81cf9639f3a44cc6baa2fe8c2c92572992b5b0
WayneZhang <waynezhang226@gmail.com>
2020-10-05 12:03:31 +0800

    basic finished EC demo

--- ErasureCode/encode.py
+++ ErasureCode/encode.py
@@ -1,6 +1,7 @@
 # This python file is used for encoding input data
 
 import numpy as np
+import sys
 
 
 class Encode():
@@ -11,10 +12,12 @@ class Encode():
         """
         self.numOfData = n
         self.numOfCoded = m
+        self.numOfDataColumns = 5000
 
     def read_data(self):
         # construct original data array and distribution matrix
-        self.myData = np.random.randint(1000, size=(self.numOfData, 1), dtype=int)
+        maxValue = 1000
+        self.myData = np.random.uniform(0, maxValue, size=(self.numOfData, self.numOfDataColumns))
         upPartMatrix = np.identity(self.numOfData, dtype=int)
         lowMatrix = np.vander(np.array([1, 2, 3, 4, 5]), self.numOfCoded, increasing=True)
         self.myMatrix = np.concatenate((upPartMatrix, lowMatrix.transpose()), axis=0)
@@ -41,11 +44,36 @@ class Check():
         print(lost)
         return lost
 
+    def array_precision_equal(self, a1, a2):
+        """
+        :param a1: input array 1
+        :param a2: input array 2
+        :return: bool, Returns True if the arrays are equal.
+        """
+        try:
+            a1, a2 = np.asarray(a1), np.asarray(a2)
+        except Exception:
+            return False
+        if a1.shape != a2.shape:
+            return False
+        decimalNum = 4     # rarely, there are some precision mismatch between some elements in arrays
+        a1 = np.around(a1, decimals=decimalNum)
+        a2 = np.around(a2, decimals=decimalNum)
+        for idx, x in np.ndenumerate(a1):
+            temp_a1 = x
+            temp_a2 = a2[idx]
+            if temp_a1 != temp_a2:
+                print("not equal element:", temp_a1, " vs ", temp_a2)
+                return False
+
+        return True
+
+
 class Decode():
     def __init__(self, lost):
         self.lost = lost.reshape(-1, 1)
 
-    def decoding(self, encoder):
+    def decoding(self, encoder, checker):
         """
         :param encoder: the instance of class Encode
         :return:
@@ -57,23 +85,26 @@ class Decode():
             # Not invertible. Skip this one.
             pass
         self.originalData = np.dot(tempInverse, self.survivors)
-        self.originalData = self.originalData.astype(int)
         print("decoded original data:\n", self.originalData)
-        if (np.array_equal(self.originalData, encoder.myData)):
+        if (checker.array_precision_equal(self.originalData, encoder.myData)):
             print("decode successfully!!")
         else:
             print("decode failed!!!")
-
-
+            sys.exit()
 
     def abandon_lost(self, encoder):
         self.survivors = np.delete(encoder.encodeResults, self.lost, 0)
         self.myMatrixNow = np.delete(encoder.myMatrix, self.lost, 0)
 
 if __name__ == "__main__":
-    encoder = Encode(5,3)
-    encoder.encoding()
-    checker = Check(3)
-    lostRows = checker.make_lost(encoder)
-    decoder = Decode(lostRows)
-    decoder.decoding(encoder)
+    i = 0
+    iteration_times = 100
+    while i < iteration_times:
+        encoder = Encode(5, 3)
+        encoder.encoding()
+        checker = Check(3)
+        lostRows = checker.make_lost(encoder)
+        decoder = Decode(lostRows)
+        decoder.decoding(encoder, checker)
+        print(i)
+        i = i+1
